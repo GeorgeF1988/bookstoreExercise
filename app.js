b7bce99a0814ae291c167268d80fcb649b8a8654
GeorgeF1988 <irafou@hotmail.com>
2020-12-05 14:32:43 +0200

    bookstore commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementsByClassName('overlay')[0].style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementsByClassName('overlay')[0].style.width = "0%";
+}
